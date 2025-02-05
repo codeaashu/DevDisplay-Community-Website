feba5dffe184f2f7912c2821099c86825d1e3e34
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-02-06 00:46:54 +0530

    dd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,7 @@ export const Navbar = () => {
               href="/"
               className="ml-2 font-bold text-xl flex items-center"
             >
-              <img src="../assets/DevDisplayLOGO/ICON.png" alt="Logo" className="h-10 w-10 mr-2" />
+              <img src="./assets/DevDisplayLOGO/ICON.png" alt="Logo" className="h-10 w-10 mr-2" />
               DevDisplay
             </a>
           </NavigationMenuItem>
