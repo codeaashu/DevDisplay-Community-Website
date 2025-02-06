94440b117c5def8ce4e06c917c2efe9472251ca2
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-02-06 17:07:27 +0530

    dd

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,7 +45,7 @@ export const Hero = () => {
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-        A dynamic hub for tech enthusiasts at GNOIT! Here, we come together to Connect ▸ Collaborate ▸ Code ▸ Create ▸ Conquer with global tech innovators through open-source projects, workshops, hackathons, and events.
+        A dynamic hub for tech enthusiasts in Colleges! Here, we come together to Connect ▸ Collaborate ▸ Code ▸ Create ▸ Conquer with global tech innovators through open-source projects, workshops, hackathons, and events.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
